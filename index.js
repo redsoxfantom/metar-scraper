1bdec8b115afca4ae552b948c32c61e6f48ec29b
Tom English <thomas.f.english@lmco.com>
2022-02-17 09:53:33 -0500

    Save off retrieved METARs

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
-const needle = require('needle')
 const config = require('./configparser')
+const metarretriever = require('./metarretriever')
 
+let retrievedData = {}
+
+metarretriever(config.airports, config.metarsource, config.refreshInterval, (update) => {
+  for (const [key, val] of Object.entries(update)) {
+    retrievedData[key] = val
+  }
+})
 
 let app = express()
+app.set('view engine', 'pug')
+app.get('/', (req, res) => {
+  
+})
 
 app.listen(8080, () => {
   console.log("Listening on port 8080")
